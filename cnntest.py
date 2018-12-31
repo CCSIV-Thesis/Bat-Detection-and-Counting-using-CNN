35b8e4a22bc01833cba8dedd10974a4c250c1b51
Charles Cariño <charlescarino4@gmail.com>
2018-12-31 22:35:19 +0800

    Realistic results done.

--- cnntest.py
+++ cnntest.py
@@ -2,6 +2,8 @@
 import os,cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import imp
+import scipy
 
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
@@ -19,7 +21,7 @@ from keras.optimizers import SGD,RMSprop,adam
 
 PATH = os.getcwd()
 # Define data path
-data_path = PATH + '\\Bat dataset'
+data_path = PATH + '\\databats'
 data_dir_list = os.listdir(data_path)
 
 img_rows=128
@@ -27,101 +29,86 @@ img_cols=128
 num_channel=3
 num_epoch=20
 
-# Define the number of classes
-num_classes = 1
-
 img_data_list=[]
+labels_name = {'bat':0,'non-bat':1}
+labels_list = []
 
-# for file in data_dir_list:
-#     print(file)
-print("Please wait...")
 for dataset in data_dir_list:
-	input_img= cv2.imread(data_path + '\\'+ dataset)
-	# Write image onto disk
-	gray = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
-	# Convert image from RGB to GRAY
-	ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-	# apply thresholding to convert the image to binary
-	fg = cv2.erode(thresh, None, iterations=1)
-	# erode the image
-	bgt = cv2.dilate(thresh, None, iterations=1)
-	# Dilate the image
-	ret, bg = cv2.threshold(bgt, 1, 128, 1)
-	# Apply thresholding
-	marker = cv2.add(fg, bg)
-	# Add foreground and background
-	canny = cv2.Canny(marker, 300, 350)
-	# Apply canny edge detector
-	new, contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	# Finding the contors in the image using chain approximation
-	marker32 = np.int32(marker)
-	# converting the marker to float 32 bit
-	cv2.watershed(input_img,marker32)
-	# Apply watershed algorithm
-	m = cv2.convertScaleAbs(marker32)
-	ret, thresh = cv2.threshold(m, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-	# Apply thresholding on the image to convert to binary image
-	thresh_inv = cv2.bitwise_not(thresh)
-	# Invert the thresh
-	res = cv2.bitwise_and(input_img, input_img, mask=thresh)
-	# Bitwise and with the image mask thresh
-	res3 = cv2.bitwise_and(input_img, input_img, mask=thresh_inv)
-	# Bitwise and the image with mask as threshold invert
-	res4 = cv2.addWeighted(res, 1, res3, 1, 0)
-	# Take the weighted average
-	final = cv2.drawContours(res4, contours, -1, (0, 255, 0), 1)
-	input_img_resize=cv2.resize(final,(128,128))
-	img_data_list.append(input_img_resize)
-	# print("Done "+dataset+"\n")
-
+	img_list=os.listdir(data_path+'\\'+ dataset)
+	print ('Loading the images of dataset-'+'{}\n'.format(dataset))
+	print ("Please wait...")
+	label = labels_name[dataset]
+	for img in img_list:
+		input_img= cv2.imread(data_path + '\\'+ dataset + '\\'+ img)
+		# Write image onto disk
+		gray = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+		# Convert image from RGB to GRAY
+		ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+		# apply thresholding to convert the image to binary
+		fg = cv2.erode(thresh, None, iterations=1)
+		# erode the image
+		bgt = cv2.dilate(thresh, None, iterations=1)
+		# Dilate the image
+		ret, bg = cv2.threshold(bgt, 1, 128, 1)
+		# Apply thresholding
+		marker = cv2.add(fg, bg)
+		# Add foreground and background
+		canny = cv2.Canny(marker, 300, 350)
+		# Apply canny edge detector
+		new, contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+		# Finding the contors in the image using chain approximation
+		marker32 = np.int32(marker)
+		# converting the marker to float 32 bit
+		cv2.watershed(input_img,marker32)
+		# Apply watershed algorithm
+		m = cv2.convertScaleAbs(marker32)
+		ret, thresh = cv2.threshold(m, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+		# Apply thresholding on the image to convert to binary image
+		thresh_inv = cv2.bitwise_not(thresh)
+		# Invert the thresh
+		res = cv2.bitwise_and(input_img, input_img, mask=thresh)
+		# Bitwise and with the image mask thresh
+		res3 = cv2.bitwise_and(input_img, input_img, mask=thresh_inv)
+		# Bitwise and the image with mask as threshold invert
+		res4 = cv2.addWeighted(res, 1, res3, 1, 0)
+		# Take the weighted average
+		final = cv2.drawContours(res4, contours, -1, (0, 255, 0), 1)
+		input_img_resize=cv2.resize(final,(128,128))
+		img_data_list.append(input_img_resize)
+		labels_list.append(label)
 
 img_data = np.array(img_data_list)
 img_data = img_data.astype('float32')
-# img_data /= 255
-print (img_data.shape)
-test_img = np.array(img_data[0])
-test_img = test_img.astype('int64')
-# print("Data list: ")
-# print(img_data)
-# print("Img: ")
-# print(test_img)
+img_data /= 255
 
-# test = cv2.imread("1.jpg",1)
-# print(test)
-print(test_img)
-# # Assigning Labels
-#
-# # Define the number of classes
-num_classes = 1
-num_of_samples = img_data.shape[0]
-labels = np.ones((num_of_samples,),dtype='int64')
-labels[0:] = 0
-names = ['bats']
-#
+num_classes = 2
+labels = np.array(labels_list)
+# print the count of number of samples for different classes
+print(np.unique(labels,return_counts=True))
 # convert class labels to on-hot encoding
 Y = np_utils.to_categorical(labels, num_classes)
 #
 #Shuffle the dataset
-x,y = shuffle(img_data,Y, random_state=2)
+x,y = shuffle(img_data,Y, random_state=10)
 # Split the dataset
-X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2)
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.25, random_state=10)
 
 #%%
 # Defining the model
 input_shape=img_data[0].shape
 print(input_shape)
 model = Sequential()
-model.add(Conv2D(32,kernel_size=(3,3),padding='same',input_shape=input_shape,activation="relu"))
-# model.add(Activation('relu'))
-model.add(Conv2D(32,kernel_size=(3,3),padding='same',activation="relu"))
-# model.add(Activation('relu'))
+model.add(Conv2D(32,kernel_size=(3,3),padding='same',input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Conv2D(32,kernel_size=(3,3),padding='same'))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2),dim_ordering="tf"))
 model.add(Dropout(0.5))
 
-model.add(Conv2D(64,kernel_size=(3,3),padding='same',activation="relu"))
-# model.add(Activation('relu'))
-model.add(Conv2D(64,kernel_size=(3,3),padding='same',activation="relu"))
-#model.add(Activation('relu'))
+model.add(Conv2D(64,kernel_size=(3,3),padding='same'))
+model.add(Activation('relu'))
+model.add(Conv2D(64,kernel_size=(3,3),padding='same'))
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2),dim_ordering="tf"))
 model.add(Dropout(0.5))
 
@@ -134,7 +121,7 @@ model.add(Activation('softmax'))
 
 #sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 #model.compile(loss='categorical_crossentropy', optimizer=sgd,metrics=["accuracy"])
-model.compile(loss='binary_crossentropy', optimizer='rmsprop',metrics=["accuracy"])
+model.compile(loss='binary_crossentropy', optimizer='adam',metrics=["accuracy"])
 #
 # # Viewing model_configuration
 #
@@ -147,28 +134,10 @@ model.layers[0].get_weights()
 np.shape(model.layers[0].get_weights()[0])
 model.layers[0].trainable
 
+print(Y)
 #%%
 # Training
 hist = model.fit(X_train, y_train, batch_size=16, epochs=20, verbose=1, validation_data=(X_test, y_test))
-
-#hist = model.fit(X_train, y_train, batch_size=32, nb_epoch=20,verbose=1, validation_split=0.2)
-
-# Training with callbacks
-from keras import callbacks
-
-filename='model_train_new.csv'
-# csv_log=callbacks.CSVLogger(filename, separator=',', append=False)
-#
-# early_stopping=callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='min')
-#
-filepath="Best-weights-my_model-{epoch:03d}-{loss:.4f}-{acc:.4f}.hdf5"
-#
-# checkpoint = callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-#
-# callbacks_list = [csv_log,early_stopping,checkpoint]
-#
-# hist = model.fit(X_train, y_train, batch_size=16, epochs=num_epoch, verbose=1, validation_data=(X_test, y_test),callbacks=callbacks_list)
-
 
 # visualizing losses and accuracy
 train_loss=hist.history['loss']
@@ -198,18 +167,22 @@ plt.grid(True)
 plt.legend(['train','val'],loc=4)
 #print plt.style.available # use bmh, classic,ggplot for big pictures
 plt.style.use(['classic'])
-
-#%%
-
-# Evaluating the model
-
-score = model.evaluate(X_test, y_test, show_accuracy=True, verbose=0)
+plt.show()
+# #%%
+#
+# # Evaluating the model
+#
+score = model.evaluate(X_test, y_test, verbose=0)
 print('Test Loss:', score[0])
 print('Test accuracy:', score[1])
 
 test_image = X_test[0:1]
-print (test_image.shape)
-
+# X_test = X_test * 255
+# X_test = X_test.astype('int64')
+# print(X_test[0:1])
+# print(X_test[0:1].shape)
+# cv2.imshow("Test Image", X_test[0:1])
+# cv2.waitKey(0)
 print(model.predict(test_image))
 print(model.predict_classes(test_image))
 print(y_test[0:1])

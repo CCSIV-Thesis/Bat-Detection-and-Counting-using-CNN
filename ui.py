0818b8f83db5e09ca4af8426bdedc7625e8d6dd9
KuyaKoya <sethmcornelio@su.edu.ph>
2019-02-28 16:41:46 +0800

    GUI

--- ui.py
+++ ui.py
@@ -52,50 +52,119 @@
 
 from tkinter import *
 
+try:
+    import tkinter as tk
+    import tkinter.ttk as ttk
+    from tkinter import filedialog
+except ImportError:
+    import Tkinter as tk
+    import ttk
+    import tkFileDialog as filedialog
+from tkfilebrowser import askopendirname, askopenfilenames, asksaveasfilename
+from PIL import ImageTk, Image
+
+
 root = Tk()
 root.resizable(False,False)
-root.geometry('500x500')
-root.title("Registration Form")
+root.geometry('1000x700')
+root.title("BAT DETECTION AND COUNTING USING CNN")
 
-label_0 = Label(root, text="Bat Counter LOL",width=20,font=("bold", 20))
-label_0.place(x=90,y=53)
+class SampleApp(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.minsize(1280,720)
+        self.button = ttk.Button(text="start", command=self.start)
+        self.button.pack()
+        self.button1 = ttk.Button(text="select file", command=self.getFile)
+        self.button1.pack()
+        self.combo = Combobox(self)
+        self.combo['values']= ("Left", "Right", "Top", "Bottom")
+        self.combo.current(0) #set the selected item
+        self.combo.pack()
+        self.progress = ttk.Progressbar(self, orient="horizontal",
+                                        length=200, mode="determinate")
+
+        self.progress.pack()
+        self.bytes = 0
+        self.maxbytes = 0
+
+    def getFile(self):
+        self.file = filedialog.askopenfilename(filetypes = (("Video files","*.mp4"),("all files","*.*")))
+        # self.dir = filedialog.askdirectory()
+
+        print(self.file)
+    def start(self):
+        self.progress["value"] = 0
+        self.maxbytes = 10000
+        self.progress["maximum"] = 10000
+        self.read_bytes()
+
+    def read_bytes(self):
+        '''simulate reading 500 bytes; update progress bar'''
+        self.bytes += 500
+        self.progress["value"] = self.bytes
+        if self.bytes == self.maxbytes:
+            messagebox.showinfo('Done!', 'Done')
+        elif self.bytes < self.maxbytes:
+            # read more bytes after 100 ms
+            self.after(100, self.read_bytes)
+
+def clicked():
+    root = SampleApp()
+
+def c_open_file_old():
+    vid_open = filedialog.askopenfilenames(parent=root, initialdir='/Users/KuyaKoya/Documents/Bat-Detection-and-Counting-using-CNN/', initialfile='',
+                                      filetypes=[("mp4", "*.mp4")])
+
+    vid_lbl.configure(text=vid_open)
+    print(vid_open)
 
 
-label_1 = Label(root, text="Video File",width=20,font=("bold", 10))
-label_1.place(x=80,y=130)
+label_0 = Label(root, text="Bat Counter",width=20,font=("bold", 30))
+label_0.place(x=310,y=10)
 
-entry_1 = Entry(root)
-entry_1.place(x=240,y=130)
 
-label_2 = Label(root, text="Email",width=20,font=("bold", 10))
-label_2.place(x=68,y=180)
+video = Label(root, text="Video File",width=20,font=("bold", 20))
+video.place(x=75,y=70)
 
-entry_2 = Entry(root)
-entry_2.place(x=240,y=180)
+vid_lbl = Label(root, text="select video")
+vid_lbl.config(state = NORMAL)
+# vid_lbl.config(state="readonly")
+vid_lbl.place(x=300, y=70)
 
-label_3 = Label(root, text="Gender",width=20,font=("bold", 10))
-label_3.place(x=70,y=230)
-var = IntVar()
-Radiobutton(root, text="Male",padx = 5, variable=var, value=1).place(x=235,y=230)
-Radiobutton(root, text="Female",padx = 20, variable=var, value=2).place(x=290,y=230)
+vid_open = Button(root, text="Open files", command=c_open_file_old)
+vid_open.place(x=300,y=100)
 
-label_4 = Label(root, text="country",width=20,font=("bold", 10))
-label_4.place(x=70,y=280)
+label_4 = Label(root, text="Direction",width=20,font=("bold", 20))
+label_4.place(x=75,y=120)
 
-list1 = ['Canada','India','UK','Nepal','Iceland','South Africa'];
+list1 = ['LEFT','RIGHT','DOWN','UP'];
 c=StringVar()
 droplist=OptionMenu(root,c, *list1)
-droplist.config(width=15)
-c.set('select your country')
-droplist.place(x=240,y=280)
+droplist.config(width=20)
+c.set('Select Direction')
+droplist.place(x=300,y=125)
 
-label_4 = Label(root, text="Programming",width=20,font=("bold", 10))
-label_4.place(x=85,y=330)
-var1 = IntVar()
-Checkbutton(root, text="java", variable=var1).place(x=235,y=330)
-var2 = IntVar()
-Checkbutton(root, text="python", variable=var2).place(x=290,y=330)
+submit = Button(root, text='Submit',width=20,bg='green',fg='green', command=clicked).place(x=410,y=180)
 
-Button(root, text='Submit',width=20,bg='brown',fg='white').place(x=180,y=380)
+
+main_frame = "batmanlogo.jpg"
+crop_frame = "b.png"
+classification = "Bat"
+
+#Creates a Tkinter-compatible photo image, which can be used everywhere Tkinter expects an image object.
+#this is for the main frame
+main_frame = ImageTk.PhotoImage(Image.open(main_frame))
+panel = tk.Label(root, image = main_frame)
+panel.place(x=150,y=450)
+
+#this is for the crop frame
+crop_frame = ImageTk.PhotoImage(Image.open(crop_frame))
+panel = tk.Label(root, image = crop_frame)
+panel.place(x=550,y=450)
+
+#this is for the classification
+label = Label(root, text=classification,width=10,font=("bold", 20)).place(x=560, y=550)
 
 root.mainloop()

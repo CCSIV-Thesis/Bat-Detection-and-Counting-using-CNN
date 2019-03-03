df7926d1642c3007559c0a80c5505abf279dc7d7
KuyaKoya <sethmcornelio@su.edu.ph>
2019-03-03 20:33:14 +0800

    removed preprocessing part in this file

--- video_testing.py
+++ video_testing.py
@@ -9,44 +9,6 @@ z = 0
 vid = cv2.VideoCapture('output.mp4')
 model = load_model("latestbat.model")
 
-def preprocessing(frame):
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Convert image from RGB to GRAY
-    ret, thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    # apply thresholding to convert the image to binary
-    fg = cv2.erode(thresh, None, iterations=1)
-    # erode the image
-    bgt = cv2.dilate(thresh, None, iterations=1)
-    # Dilate the image
-    ret, bg = cv2.threshold(bgt, 1, 128, 1)
-    # Apply thresholding
-    marker = cv2.add(fg, bg)
-    # Add foreground and background
-    canny = cv2.Canny(marker, 10, 15)
-    # Apply canny edge detector
-    new, contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # Finding the contors in the image using chain approximation
-    marker32 = np.int32(marker)
-    # converting the marker to float 32 bit
-    cv2.watershed(frame,marker32)
-    # Apply watershed algorithm
-    m = cv2.convertScaleAbs(marker32)
-    ret, thresh = cv2.threshold(m, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-    # Apply thresholding on the image to convert to binary image
-    thresh_inv = cv2.bitwise_not(thresh)
-    # Invert the thresh
-    res = cv2.bitwise_and(frame, frame, mask=thresh)
-    # Bitwise and with the image mask thresh
-    res3 = cv2.bitwise_and(frame, frame, mask=thresh_inv)
-    # Bitwise and the image with mask as threshold invert
-    res4 = cv2.addWeighted(res, 1, res3, 1, 0)
-    # Take the weighted average
-
-    final = cv2.drawContours(res4, contours, -1, (0, 255, 0), 1)
-    # print("Preprocessed image shape: ",final.shape)
-    # cv2.imwrite("Final img.png",final)
-    return final
-
 def predictions(frame,batCounter,direction):
     print(frame.shape)
     IMG_SIZE = 64
@@ -146,7 +108,6 @@ print("Processing....")
 while (vid.isOpened()):
     if(z < vid.get(cv2.CAP_PROP_FRAME_COUNT)):
         ret, frame = vid.read()
-        # preprocessedFrame = preprocessing(frame)
         frameCount = predictions(frame,batCounter,direction)
         finalCount = finalCount + frameCount
         z = z + 1
